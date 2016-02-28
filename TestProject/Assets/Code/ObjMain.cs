6374129ab39654092b30510be3680fa6869021b1
David Tran <dt_dbqt@hotmail.com>
2016-02-27 16:13:57 -0800

    initial unity project

---
+++ TestProject/Assets/Code/ObjMain.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjMain : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Mesh holderMesh = new Mesh();
+        ObjectImporter newMesh = new ObjectImporter();
+        holderMesh = newMesh.ImportFile("D:/Personal_Data/Projects/Unity/3dtest/nwhacks16/TestProject/Assets/3dmodel/teapot.obj");
+
+        MeshRenderer renderer = gameObject.AddComponent<MeshRenderer>();
+        MeshFilter filter = gameObject.AddComponent<MeshFilter>();
+        filter.mesh = holderMesh;
+	}
+	
+}
