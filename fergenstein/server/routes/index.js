1b6696129a6509855593e25289b0af8b38545dea
Navjot <NavjotSPanesar@gmail.com>
2016-02-28 00:18:48 -0800

    Front end go

--- fergenstein/server/routes/index.js
+++ fergenstein/server/routes/index.js
@@ -2,14 +2,10 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+var multer  =   require('multer');
+
 
 router.get('/getobj', function(req, res, next) {
-  
-
 	fs.readFile('teapot.obj', 'utf8', function (err,data) {
 	  if (err) {
 	    return console.log(err);
@@ -18,6 +14,30 @@ router.get('/getobj', function(req, res, next) {
 	  res.end();
 	});
 });
+
+
+//file upload stuff
+var storage =   multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, './');
+  },
+  filename: function (req, file, callback) {
+    callback(null, "doot.obj");
+  }
+});
+
+var upload = multer({ storage : storage}).single('userPhoto');
+
+router.post('/upl',function(req,res){
+    upload(req,res,function(err) {
+        if(err) {
+          console.log(err);
+          return res.end("Error uploading file.");
+        }
+        res.redirect("success.html");
+    });
+});
+
 
 module.exports = router;
              
