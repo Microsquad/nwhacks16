89560b48dff176aee7058721c127bd58ec4a1fda
topangad <olga.bool@yahoo.ca>
2016-02-27 19:17:07 -0800

    Implemented rotation animation unity3d

---
+++ TestProject/Assets/Code/RotationAnimation.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotationAnimation : MonoBehaviour {
+
+    public float speed = 10f;
+    
+    void Start()
+    {
+        Vector3 x = this.gameObject.GetComponent<Renderer>().bounds.center;
+        this.gameObject.transform.Translate(-x);
+    }
+	// Update is called once per frame
+	void Update () {
+        
+        this.gameObject.transform.Rotate(Vector3.up * speed * Time.deltaTime, Space.World);
+	}
+}
